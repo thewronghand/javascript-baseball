c7718772ef58061d24329b0e4be491e1a8f9e9a9
thewronghand <penfreak77@gmail.com>
2022-11-25 14:43:44 +0900

    feat: 정답 대조 및 볼, 스트라이크 산출 기능 구현
    입력값과 정답을 대조하여 볼, 스트라이크 갯수를 출력하는 기능을 구현한다.

---
+++ src/Model/BaseballGame.js
@@ -0,0 +1,33 @@
+const {
+  BaseballGameAnswerGenerator
+} = require('../utils/baseballGameNumberGenerator');
+
+class BaseballGame {
+  #answer;
+
+  setNewAnswer() {
+    this.#answer = BaseballGameAnswerGenerator.generate(
+      NUMBER.MIN,
+      NUMBER.MAX,
+      NUMBER.VALID_LENGTH
+    );
+  }
+
+  getMatchedBallStrikeNumber(number) {
+    let strike = 0;
+    let ball = 0;
+    const userInput = number.split('').map((element) => Number(element));
+    userInput.forEach((element, idx) => {
+      if (this.#answer[idx] === element) {
+        strike += 1;
+        return;
+      }
+      if (this.#answer.includes(element)) {
+        ball += 1;
+      }
+    });
+    return [ball, strike];
+  }
+}
+
+module.exports = BaseballGame;
