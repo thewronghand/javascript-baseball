6e2911588cbdbab5ab5e4627390958db1803a3b1
thewronghand <penfreak77@gmail.com>
2022-11-26 13:22:38 +0900

    refactor: MESSAGE 문자열 내 상수 사용문 리팩터링

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,8 +9,7 @@ const MESSAGE = {
   GAME_START: '숫자 야구 게임을 시작합니다.',
   GAME_FINISH: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
   READ_USER_INPUT_NUMBER: '숫자를 입력해주세요 : ',
-  READ_USER_INPUT_COMMAND:
-    '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
+  READ_USER_INPUT_COMMAND: `게임을 새로 시작하려면 ${KEY.RESTART}, 종료하려면 ${KEY.QUIT}를 입력하세요.\n`
 };
 
 const RESULT = {
@@ -24,4 +23,9 @@ const KEY = {
   QUIT: '2'
 };
 
-module.exports = { NUMBER, MESSAGE, RESULT, KEY };
+const ERROR = {
+  USER_INPUT_NUMBERS_INVALID: `[ERROR] 입력 숫자는 ${NUMBER.MIN}~${NUMBER.MAX} 사이의 중복되지 않는 ${NUMBER.VALID_LENGTH} 개의 숫자여야 합니다.`,
+  USER_INPUT_COMMAND_INVALID: `[ERROR] 명령어는 ${KEY.RESTART} 혹은 ${KEY.QUIT}만 입력할 수 있습니다.`
+};
+
+module.exports = { NUMBER, MESSAGE, RESULT, KEY, ERROR };
