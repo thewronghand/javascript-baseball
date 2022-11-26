4db0920bd5dbf79ca6ba184f6833055781a700f1
thewronghand <penfreak77@gmail.com>
2022-11-26 20:27:10 +0900

    refactor: MESSAGE, ERROR 객체 리팩터링

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,13 +5,6 @@ const NUMBER = {
   strikeOutCount: 3
 };
 
-const MESSAGE = {
-  gameStart: '숫자 야구 게임을 시작합니다.',
-  gameFinish: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
-  readUserInputNumbers: '숫자를 입력해주세요 : ',
-  readUserInputCommand: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
-};
-
 const RESULT = {
   ball: '볼',
   strike: '스트라이크',
@@ -23,10 +16,16 @@ const KEY = {
   quit: '2'
 };
 
+const MESSAGE = {
+  gameStart: '숫자 야구 게임을 시작합니다.',
+  gameFinish: `${NUMBER.validLength}개의 숫자를 모두 맞히셨습니다! 게임 종료`,
+  readUserInputNumbers: '숫자를 입력해주세요 : ',
+  readUserInputCommand: `게임을 새로 시작하려면 ${KEY.restart}, 종료하려면 ${KEY.quit}를 입력하세요.\n`
+};
+
 const ERROR = {
-  userInputNumbersInvalid:
-    '[ERROR] 입력 숫자는 1~9 사이의 중복되지 않는 세 개의 숫자여야 합니다.',
-  userInputCommandInvalid: '[ERROR] 명령어는 1이나 2만 입력할 수 있습니다.'
+  userInputNumbersInvalid: `[ERROR] 입력 숫자는 ${NUMBER.min}~${NUMBER.max} 사이의 중복되지 않는 ${NUMBER.validLength}개의 숫자여야 합니다.`,
+  userInputCommandInvalid: `[ERROR] 명령어는 ${KEY.restart}이나 ${KEY.quit}만 입력할 수 있습니다.`
 };
 
 module.exports = { NUMBER, MESSAGE, RESULT, KEY, ERROR };
